bfeb2651991b074f09dd293ec96951d2fbde83c5
AKIRA.M <akira@akira.ai>
2026-05-04 08:38:37 +0900

    data: 2026-05-03T23:38:37Z

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ window.__YT_DATA__ = {
   "subscribers": {
     "current": 1657,
     "previous": 1657,
-    "updated_at": "2026-05-04T08:08:31.084140+09:00",
+    "updated_at": "2026-05-04T08:38:32.406914+09:00",
     "base_at": "2026-05-02T20:47:19.949+09:00"
   },
   "history": [
@@ -1463,7 +1463,7 @@ window.__YT_DATA__ = {
       "n": 1657
     },
     {
-      "t": "2026-05-04T08:08:31.084140+09:00",
+      "t": "2026-05-04T08:38:32.406914+09:00",
       "n": 1657
     }
   ],
@@ -1472,7 +1472,7 @@ window.__YT_DATA__ = {
       "id": "1N_kPwqIT_k",
       "title": "Nikon ZR | Kamakura Sakura in Japan (4K)",
       "views": 538,
-      "views_24h": 5,
+      "views_24h": 4,
       "published_at": "2026-03-16T21:00:10+09:00",
       "thumbnail": "https://i.ytimg.com/vi/1N_kPwqIT_k/maxresdefault.jpg"
     },

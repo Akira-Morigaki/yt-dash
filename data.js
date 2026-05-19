6f37ff20facee1964d4b0895a7b37b8d8a2114c5
AKIRA.M <akira@akira.ai>
2026-05-19 19:08:38 +0900

    data: 2026-05-19T10:08:38Z

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ window.__YT_DATA__ = {
   "subscribers": {
     "current": 1671,
     "previous": 1671,
-    "updated_at": "2026-05-19T18:38:34.530110+09:00",
+    "updated_at": "2026-05-19T19:08:34.575575+09:00",
     "base_at": "2026-05-19T18:05:53.016+09:00"
   },
   "history": [
@@ -1463,7 +1463,7 @@ window.__YT_DATA__ = {
       "n": 1670
     },
     {
-      "t": "2026-05-19T18:38:34.530110+09:00",
+      "t": "2026-05-19T19:08:34.575575+09:00",
       "n": 1671
     }
   ],
@@ -1480,7 +1480,7 @@ window.__YT_DATA__ = {
       "id": "KCfcRhp_xsM",
       "title": "Nikon ZR | Cinematic Kyoto",
       "views": 3728,
-      "views_24h": 4,
+      "views_24h": 3,
       "published_at": "2026-02-11T19:00:26+09:00",
       "thumbnail": "https://i.ytimg.com/vi/KCfcRhp_xsM/maxresdefault.jpg"
     },
